23d9a46da1def64bbc3e406e19e0ddbb0fd662ff
LOADCRCHS <276597260@QQ.COM>
2019-02-26 23:43:50 +0800

    init

--- src/main/java/com/teachermate/controller/ApiV1Controller.java
+++ src/main/java/com/teachermate/controller/ApiV1Controller.java
@@ -1,24 +1,43 @@
 package com.teachermate.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @RequestMapping("/api/v1/")
-@Controller
+@RestController
 public class ApiV1Controller {
-    @ResponseBody()
-    @RequestMapping("class-attendance")
-    public Map<String, Map> class_attendance() {
+    @RequestMapping(value = "class-attendance", method =  RequestMethod.POST)
+    public Map<String, Map> class_attendance_post(){
         Map<String, Map> result = new HashMap<>();
-        Map<String, Integer> sign = new HashMap<>();
+        Map<String, Object> sign = new HashMap<>();
         sign.put("id", 1436707);
         sign.put("ttl", 299);
         result.put("sign", sign);
+        return result;
+    }
+
+
+    @RequestMapping(value = "class-attendance/{courseid}", method = RequestMethod.GET)
+    public Map<String, Map> class_attendance_get(@PathVariable Integer id, @RequestParam("courseid") Integer courseId) {
+        Map<String, Map> result = new HashMap<>();
+        Map<String, Object> sign = new HashMap<>();
+        if(id!=null){
+            sign.put("isGps",0);
+            sign.put("isQr",false);
+            sign.put("ttl",291);
+            sign.put("count",0);
+            Map<String, Object> course = new HashMap<>();
+            course.put("students",new ArrayList<>());
+            course.put("teams",new ArrayList<>());
+            course.put("signInLogs",new ArrayList<>());
+            result.put("sign", sign);
+            result.put("course", course);
+            return result;
+        }
         return result;
     }
 }
