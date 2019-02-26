78bc2b128a42d3a556139e1e8d806949962f5da4
LOADCRCHS <276597260@QQ.COM>
2019-02-26 23:44:43 +0800

    init

--- src/main/java/com/teachermate/controller/ApiV1Controller.java
+++ src/main/java/com/teachermate/controller/ApiV1Controller.java
@@ -1,6 +1,5 @@
 package com.teachermate.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,8 +9,8 @@ import java.util.Map;
 @RequestMapping("/api/v1/")
 @RestController
 public class ApiV1Controller {
-    @RequestMapping(value = "class-attendance", method =  RequestMethod.POST)
-    public Map<String, Map> class_attendance_post(){
+    @RequestMapping(value = "class-attendance", method = RequestMethod.POST)
+    public Map<String, Map> class_attendance_post() {
         Map<String, Map> result = new HashMap<>();
         Map<String, Object> sign = new HashMap<>();
         sign.put("id", 1436707);
@@ -25,15 +24,15 @@ public class ApiV1Controller {
     public Map<String, Map> class_attendance_get(@PathVariable Integer id, @RequestParam("courseid") Integer courseId) {
         Map<String, Map> result = new HashMap<>();
         Map<String, Object> sign = new HashMap<>();
-        if(id!=null){
-            sign.put("isGps",0);
-            sign.put("isQr",false);
-            sign.put("ttl",291);
-            sign.put("count",0);
+        if (id != null) {
+            sign.put("isGps", 0);
+            sign.put("isQr", false);
+            sign.put("ttl", 291);
+            sign.put("count", 0);
             Map<String, Object> course = new HashMap<>();
-            course.put("students",new ArrayList<>());
-            course.put("teams",new ArrayList<>());
-            course.put("signInLogs",new ArrayList<>());
+            course.put("students", new ArrayList<>());
+            course.put("teams", new ArrayList<>());
+            course.put("signInLogs", new ArrayList<>());
             result.put("sign", sign);
             result.put("course", course);
             return result;
