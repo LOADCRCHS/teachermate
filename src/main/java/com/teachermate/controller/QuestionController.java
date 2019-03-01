81324bc004b1112bafd918f073817c53dfca39ef
LOADCRCHS <276597260@QQ.COM>
2019-03-01 23:59:10 +0800

    init

--- src/main/java/com/teachermate/controller/QuestionController.java
+++ src/main/java/com/teachermate/controller/QuestionController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -12,7 +13,11 @@ import java.util.Map;
 public class QuestionController {
     @RequestMapping(value = "get_answer_detail/{id}",method = RequestMethod.GET)
     public Map<String, Object> get_answer_detail(@PathVariable Integer id, Integer question_id) {
-        //2019-02-27 todo 返回的格式修改
-        return null;
+        // todo 返回的格式修改
+        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> sign = new HashMap<>();
+        sign.put("count",1);
+        result.put("sign",sign);
+        return result;
     }
 }
