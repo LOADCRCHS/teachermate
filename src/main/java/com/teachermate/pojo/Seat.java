069a6704e454edd4c3c4e442bc4c8442e31ddc10
LOADCRCHS <276597260@QQ.COM>
2019-03-30 15:17:40 +0800

    seat pojo init

--- src/main/java/com/teachermate/pojo/Seat.java
+++ src/main/java/com/teachermate/pojo/Seat.java
@@ -1,4 +1,11 @@
 package com.teachermate.pojo;
 
+import lombok.Data;
+
+@Data
 public class Seat {
+    private Integer id;
+    private Integer courseId;
+    private String seatArray;
+    private String courseAdd;
 }
