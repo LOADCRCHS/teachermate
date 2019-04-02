b3499c358c5dfaa3971ea13add8e9e6af039919b
LOADCRCHS <276597260@QQ.COM>
2019-04-02 22:00:04 +0800

    fix seat get by courseId

--- src/main/java/com/teachermate/controller/SeatController.java
+++ src/main/java/com/teachermate/controller/SeatController.java
@@ -1,20 +1,23 @@
 package com.teachermate.controller;
 
+import com.teachermate.pojo.Seat;
 import com.teachermate.service.SeatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class SeatController {
-
+    @Autowired
     private SeatService seatService;
 
     @RequestMapping("getCurrentSeatInfo")
-    public Map getCurrentSeatInfo(Integer courseId){
+    public List<Seat> getCurrentSeatInfo(Integer courseId){
 
-        return null;
+        return seatService.selectByCourseId(courseId);
     }
 
     @RequestMapping("save_seat")
